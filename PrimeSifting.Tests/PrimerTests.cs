531fa14ad74f007296d3926ffb743f6abd28f4f9
Kyle Crawford <kcraw217@gmail.com>
2023-05-03 12:07:57 -0700

    add PrimeIt and RemoveUnprimes functions
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>
    dquote> co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- PrimeSifting.Tests/PrimerTests.cs
+++ PrimeSifting.Tests/PrimerTests.cs
@@ -1,12 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using PrimeSifting.models;
+using System.Collections.Generic;
+using PrimeSifting.Models;
 
 namespace PrimeSifting.Tests
 {
   [TestClass]
   public class PrimerTests
   {
-    
+    [TestMethod]
+    public void Arrayer_ReturnsArrayFromTwoToN_IntArray()
+    {
+      int input = 11;
+      int expected = 2;
+      Primer.Arrayer(input);
+      Assert.AreEqual(expected, Primer.Numbers[0]);
+    }
+
+    [TestMethod]
+    public void Primes_ReturnsPrimeNumbers_Int()
+    {
+      int expected = 2;
+      Assert.AreEqual(expected, Primer.Primes[0]);
+    }
   }
 }
--- PrimeSifting/Models/Primer.cs
+++ PrimeSifting/Models/Primer.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace PrimeSifting.Models
 {
-  class Primer
+  public class Primer
   {
+    public static int InputNum { get; set; }
+    public static List<int> Numbers { get; set; } = new List<int> { };
+    public static List<int> Primes { get; set; } = new List<int> { };
+    public static void Arrayer(int num)
+    {
+      InputNum = num;
+      for (int i = 2; i <= num; i++)
+      {
+        Numbers.Add(i);
+      }
+      PrimeIt();
+    }
 
+    public static void PrimeIt()
+    {
+      Primes.Add(Numbers[0]);
+    }
+
+    public static void RemoveUnprimes()
+    {
+      int p = Primes.Last();
+      List<int> multiples = new List<int> { };
+      foreach (int num in Numbers)
+      {
+        int nonprime = num * p;
+        if (nonprime < InputNum)
+        {
+          multiples.Add(num * p);
+        }
+        else
+        {
+          break;
+        }
+      }
+      foreach (int num in multiples)
+      {
+        foreach (int otherNum in Numbers)
+        {
+          if (num == otherNum)
+          {
+            Numbers.Remove(otherNum);
+            break;
+          }
+        }
+      }
+      PrimeIt();
+    }
   }
 }
